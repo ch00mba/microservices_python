986cffbda34204b4afa517ced63c2110199d8866
ch00mba <karyagin.finworks@gmail.com>
2021-08-28 19:48:47 +0300

    Update recommendations.py

--- recommendations/recommendations.py
+++ recommendations/recommendations.py
@@ -34,3 +34,37 @@ books_by_category = {
         BookRecommendation(id=9, title="Man's Search for Meaning"),
     ],
 }
+
+
+
+class RecommendationService(
+    recommendations_pb2_grpc.RecommendationsServicer
+):
+
+    def Recommend(self, request, context):
+        if request.category not in books_by_category:
+            context.abort(grpc.StatusCode.NOT_FOUND, "Category not found")
+
+        books_for_category = books_by_category[request.category]
+        num_results = min(request.max_results, len(books_for_category))
+        books_to_recommend = random.sample(
+            books_for_category, num_results
+        )
+
+        return RecommendationResponse(recommendations=books_to_recommend)
+
+
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    recommendations_pb2_grpc.add_RecommendationsServicer_to_server(
+        RecommendationService(), server
+    )
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
