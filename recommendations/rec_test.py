1d89a1f6c3a9ef2d87e67e51f7fea14a886be933
ch00mba <karyagin.finworks@gmail.com>
2021-08-28 19:36:33 +0300

    Update rec_test.py

--- recommendations/rec_test.py
+++ recommendations/rec_test.py
@@ -23,3 +23,6 @@ channel = grpc.insecure_channel("localhost:50051")
 client = RecommendationsStub(channel)
 request = RecommendationRequest(user_id=1, category=BookCategory.SCIENCE_FICTION, max_results=3)
 client.Recommend(request)
+
+
+
